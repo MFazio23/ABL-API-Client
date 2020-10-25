269e7d6ac184a82dd13a7f3bf88ae8f83a9febe9
Michael Fazio <mfazio@skylinetechnologies.com>
2020-10-25 17:32:54 -0500

    Add bintray config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.4.10"
     `java-library`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 repositories {
@@ -9,8 +10,12 @@ repositories {
 }
 
 group = "dev.mfazio.abl"
-version = "0.1.0"
+version = "1.0.0"
 
+val artifactName = project.name
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+val publicationName = project.name
 
 dependencies {
     val retrofitVersion = "2.9.0"
@@ -25,15 +30,35 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
+bintray {
+    user = System.getenv("bintrayUser")
+    key = System.getenv("bintrayKey")
+    publish = true
+
+    pkg.apply {
+        repo = "android-baseball-league"
+        name = artifactName
+        userOrg = "mfazio23"
+        vcsUrl = "https://github.com/MFazio23/ABL-API-Client"
+        setLicenses("Apache-2.0")
+        setPublications(publicationName)
+    }
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
+
 publishing {
     publications {
-        create<MavenPublication>("default") {
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven {
-            url = uri("$buildDir/repository")
+        create<MavenPublication>(publicationName) {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            from(components["kotlin"])
+
+            artifact(sourcesJar)
         }
     }
 }
